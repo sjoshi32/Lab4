224fcb62a95596bcaa8cc6b3a532a5b89e318db4
sjoshi <sjoshi@wirelessprv-10-195-180-88.near.illinois.edu>
2017-09-27 14:38:38 -0500

    Lab 4 commit 9/27/17

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,46 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+/**this project needs to be fixed.
+ *
+ */
+public class ReclamationProject {
+    /**
+     * Fix this complete mess of a project :).
+     * @param a1 a1 string
+     * @param b1 b1 string
+     * @return r  r string
+     */
+    public static String doit(final String a1, final String b1) {
+        String a = a1;
+        String b = b1;
+        if (a.length() > b.length()) {
+            String c = "null";
+            c = a;
+            a = b;
+            b = c;
+            }
+        String r = "null";
+       // r = (a.equals(b)) ? "" : "";
+        if (a.equals(b)) {
+            r = "";
+        } else {
+            r = "";
+        }
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    if (a.regionMatches(i, b, k, j) && j > r.length()) {
+                        r = a.substring(i, i + j);
+                    }
+
+                        }
+                } // Ah yeah
+        }
+        return r;
+       }
+
 }
